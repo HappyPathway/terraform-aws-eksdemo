d5e448afc175d84fd064d219c4586657e219486f
Dave Arnold <dave.arnold@clover.com>
2021-12-03 13:00:47 -0800

    adding node group to cluser

--- eks.tf
+++ eks.tf
@@ -21,3 +21,30 @@ resource "aws_eks_cluster" "eks" {
   #   service_ipv4_cidr = data.aws_subnet.selected.cidr_block
   # }
 }
+
+
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks-node-group-${local.app_name}"
+  node_role_arn   = aws_iam_role.eks_nodes.arn # aws_iam_role" "eks_nodes
+  subnet_ids      = module.public-subnet.*.subnet_id
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 2
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-node-group-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-node-group-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-node-group-AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
