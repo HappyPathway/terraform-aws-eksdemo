6978e09441e5dd090cb996ed4876565f1c565a56
Dave Arnold <dave.arnold@clover.com>
2021-12-03 12:10:01 -0800

    fixing key name for bastion

--- bastion.tf
+++ bastion.tf
@@ -2,7 +2,7 @@ module "bastion" {
   source           = "app.terraform.io/roknsound/bastion/aws"
   version          = "~> 1.0"
   admin_sg         = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
-  key_name         = var.key_name
+  key_name         = aws_key_pair.deployer.key_name
   network_name     = var.network_name
   public_subnet_id = element(module.public-subnet.*.subnet_id, 0)
   ssh_access       = "0.0.0.0/0"
