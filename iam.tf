f541d30b151a3be31f55c062e8ddccf5cec5eaa9
Dave Arnold <dave.arnold@clover.com>
2021-12-02 16:03:22 -0800

    refactoring some stuff

--- iam.tf
+++ iam.tf
@@ -19,12 +19,12 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks.name
+  role       = aws_iam_role.air.name
 }
 
 # Optionally, enable Security Groups for Pods
 # Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
 resource "aws_iam_role_policy_attachment" "eks-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.eks.name
+  role       = aws_iam_role.air.name
 }
