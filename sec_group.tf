a895b81c630b7d2d4d8157300a3689d058ba8dc8
Dave Arnold <dave.arnold@clover.com>
2021-12-02 16:42:45 -0800

    whitelisting all ip addresses... .bad security, good demo

--- sec_group.tf
+++ sec_group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group_rule" "sec" {
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = data.tfe_ip_ranges.addresses.api
+  cidr_blocks = "0.0.0.0/0" # data.tfe_ip_ranges.addresses.api
   # ipv6_cidr_blocks  = [aws_vpc.example.ipv6_cidr_block]
   security_group_id = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
 }
